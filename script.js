cb700a27bf8ebba9fc6adf2c0ccd53d513787033
lakshay0501 <79932200+lakshay0501@users.noreply.github.com>
2024-05-31 11:31:52 +0530

    working project

--- script.js
+++ script.js
@@ -3,24 +3,8 @@ function getInputValue() {
     return document.getElementById("inputbox").value;
 }
 
-async function fetchData() {
-    const response = await fetch("https://pbivizedit.com/api/visuals");
-
-    const data = await response.json()
-
-    const displayData = data.items;
-
-    // console.log(displayData)
-
-    let search = getInputValue().toLowerCase();
-
-    const filteredData = displayData.filter(item => item.name.toLowerCase().startsWith(search));
-
-    console.log(filteredData)
-
-    document.getElementById("content").innerHTML = ''
-
-    filteredData?.map((item) => {
+function renderData(filteredData) {
+    filteredData.forEach((item) => {
         const card = document.createElement("div")
         card.className = "card"
 
@@ -39,6 +23,49 @@ async function fetchData() {
 
         document.getElementById("content").appendChild(card);
     })
+}
+
+function clearDOM() {
+    const content = document.getElementById("content");
+
+    while (content.firstChild) {
+        content.removeChild(content.firstChild)
+    }
+}
+
+
+async function fetchDataFromApi() {
+    const response = await fetch("https://pbivizedit.com/api/visuals");
+
+    const data = await response.json()
+
+    const displayData = data.items;
+
+    return displayData;
+}
+
+
+
+async function fetchData() {
+    // const response = await fetch("https://pbivizedit.com/api/visuals");
+
+    // const data = await response.json()
+
+    const displayData = await fetchDataFromApi();
+
+    console.log(displayData)
+
+    let search = getInputValue().toLowerCase();
+
+    const filteredData = displayData.filter(item => item.name.toLowerCase().includes(search));
+
+    // console.log(filteredData)
+
+    // document.getElementById("content").innerHTML = ''
+
+    clearDOM()
+
+    renderData(filteredData)
 }
 
 fetchData();
