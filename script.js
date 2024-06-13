b2b3fee35fe1ddb6102d7ae61f29c65a93c17df8
lakshay0501 <79932200+lakshay0501@users.noreply.github.com>
2024-06-13 12:33:36 +0530

    modified the code for index.js and script.js

--- script.js
+++ script.js
@@ -1,72 +1,61 @@
+const filter = fetchData();
+onSearchClick();
 
-function getInputValue() {
-    return document.getElementById("inputbox").value;
+function createCard(item) {
+    const card = document.createElement("div");
+    card.className = "card";
+
+    const cardTitle = document.createElement("h3");
+    cardTitle.textContent = item.name;
+
+    const cardDescription = document.createElement("p");
+    cardDescription.textContent = item.description;
+
+    const cardImage = document.createElement("img");
+    cardImage.src = item.imagePath;
+
+    card.appendChild(cardImage);
+    card.appendChild(cardTitle);
+    card.appendChild(cardDescription);
+    return card;
 }
 
-function renderData(filteredData) {
-    filteredData.forEach((item) => {
-        const card = document.createElement("div")
-        card.className = "card"
-
-        const cardTitle = document.createElement("h3")
-        cardTitle.textContent = item.name
-
-        const cardDescription = document.createElement("p")
-        cardDescription.textContent = item.description
-
-        const cardImage = document.createElement("img")
-        cardImage.src = item.imagePath
-
-        card.appendChild(cardImage);
-        card.appendChild(cardTitle)
-        card.appendChild(cardDescription)
-
-        document.getElementById("content").appendChild(card);
-    })
+async function onSearchClick() {
+    const searchText = getInputValue("inputbox").toLowerCase();
+    let container = document.getElementById("content");
+    const filteredData = await filter.filter(searchText);
+    displayItems(container, filteredData, createCard);
 }
 
-function clearDOM() {
-    const content = document.getElementById("content");
+function displayItems(container, items, cardFactory) {
+    clearDOM(container);
+    items.forEach(item => {
+        const card = cardFactory(item);
+        container.appendChild(card);
+    });
+}
 
-    while (content.firstChild) {
-        content.removeChild(content.firstChild)
+function clearDOM(container) {
+    while (container.firstChild) {
+        container.removeChild(container.firstChild);
     }
 }
 
-
-async function fetchDataFromApi() {
-    const response = await fetch("https://pbivizedit.com/api/visuals");
-
-    const data = await response.json()
-
-    const displayData = data.items;
-
-    return displayData;
+function getInputValue(itemId) {
+    return document.getElementById(itemId).value;
 }
 
+function fetchData() {
+    let data = null;
 
-
-async function fetchData() {
-    // const response = await fetch("https://pbivizedit.com/api/visuals");
-
-    // const data = await response.json()
-
-    const displayData = await fetchDataFromApi();
-
-    console.log(displayData)
-
-    let search = getInputValue().toLowerCase();
-
-    const filteredData = displayData.filter(item => item.name.toLowerCase().includes(search));
-
-    // console.log(filteredData)
-
-    // document.getElementById("content").innerHTML = ''
-
-    clearDOM()
-
-    renderData(filteredData)
+    return {
+        async filter(searchText) {
+            if (!data) {
+                const response = await fetch("https://pbivizedit.com/api/visuals");
+                const json = await response.json();
+                data = json.items;
+            }
+            return (data ?? []).filter(item => item.name?.toLowerCase().includes(searchText ?? ""));
+        }
+    }
 }
-
-fetchData();
-
